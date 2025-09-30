0cab98f5eeb8a95668d003fa7053fffaf7f1f087
HTRMC <143692436+HTRMC@users.noreply.github.com>
2025-09-30 08:34:34 +0200

    Add Vulkan instance creation to application

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 854;
 const uint32_t HEIGHT = 480;
@@ -18,20 +20,51 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance; // Vulkan instance handle
 
     void initWindow() {
         glfwInit();
 
-        // Tell GLFW not to create an OpenGL context
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        // Disable resizing (for now)
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "MinecraftClone", nullptr, nullptr);
     }
 
     void initVulkan() {
-        // Will call Vulkan setup functions in later steps
+        createInstance();
+    }
+
+    void createInstance() {
+        // 1. Application info
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // 2. Instance create info
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // 3. Extensions (GLFW tells us what it needs)
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // 4. Validation layers (none yet)
+        createInfo.enabledLayerCount = 0;
+
+        // 5. Create the instance
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create Vulkan instance!");
+        }
     }
 
     void mainLoop() {
@@ -41,6 +74,9 @@ private:
     }
 
     void cleanup() {
+        // Destroy Vulkan instance before quitting
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
         glfwTerminate();
     }
